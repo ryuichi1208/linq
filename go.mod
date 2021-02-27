a50d100c9a36c8fbba05e70762dc3a2668e48b7e
ryuichi1208 <ryucrosskey@gmail.com>
2021-02-27 20:39:37 +0900

    single modeを追加

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module linq
 go 1.15
 
 require (
+	github.com/logrusorgru/aurora/v3 v3.0.0 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- linq.go
+++ linq.go
@@ -4,24 +4,28 @@ import (
 	"context"
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"os/signal"
 	"runtime/debug"
 	"strings"
+	"syscall"
 	"time"
 
-	"gopkg.in/yaml.v3"
-
+	. "github.com/logrusorgru/aurora/v3"
 	"golang.org/x/sync/errgroup"
+	"gopkg.in/yaml.v3"
 )
 
 var (
 	fileName    = flag.String("f", "./test.yml", "読み込ませたいyamlファイルのパス")
 	worknums    = flag.Int("w", 6, "実行するhttpクライアントの並列数")
+	single      = flag.Bool("s", false, "シングルフライトを用いた実装")
 	verbosemode = flag.Bool("v", false, "詳細表示モード")
 )
 
@@ -49,8 +53,9 @@ func readYaml(filename string) Url {
 		log.Fatal(err)
 	}
 	if *verbosemode {
-		log.Println(filename)
-		log.Println(len(u.Url), u)
+		log.Println(Bold(Cyan(fmt.Sprintf("[DEBUG]: %s", filename))))
+		log.Println(Bold(Cyan(len(u.Url))))
+		log.Println(Bold(Cyan(u)))
 	}
 	return u
 
@@ -64,6 +69,7 @@ func worker(ch <-chan string, id int, cli *http.Client, ctx context.Context) err
 				if *verbosemode {
 					req, _ := http.NewRequest("GET", url, nil)
 					header, _ := httputil.DumpRequestOut(req, true)
+					// httpのヘッダー情報をdump
 					log.Println(string(header))
 				}
 				resp, err := cli.Get(url)
@@ -81,8 +87,26 @@ func worker(ch <-chan string, id int, cli *http.Client, ctx context.Context) err
 	}
 }
 
+func workerSingleFlyght(ch <-chan string, id int, cli *http.Client) {
+	return
+}
+
 func main() {
 	flag.Parse()
+	flag.Usage = func() {
+		usageTxt := `Usage example [option]
+An example of customizing usage output
+
+    -s, --s  STRING argument, default: String help message
+    -i, --i  INTEGER argument, default: Int help message
+    -b, --b  BOOLEAN argument, default: Bool help message
+`
+		fmt.Fprintf(os.Stderr, "%s\n", usageTxt)
+	}
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
 	urls := readYaml(*fileName)
 
 	if *verbosemode {
@@ -101,13 +125,21 @@ func main() {
 	}
 
 	eg, ctx := errgroup.WithContext(context.TODO())
-	for i := 0; i < *worknums; i++ {
-		i := i
-		// 1つでも接続エラーが合った場合は全てのgoroutineをキャンセルする
-		eg.Go(func() error {
-			return worker(ch, i, cli, ctx)
+	if !*single {
+		for i := 0; i < *worknums; i++ {
+			i := i
+			// 1つでも接続エラーが合った場合は全てのgoroutineをキャンセルする
+			eg.Go(func() error {
+				return worker(ch, i, cli, ctx)
 
-		})
+			})
+		}
+	} else {
+		log.Println("single flyght")
+		for i := 0; i < *worknums; i++ {
+			i := i
+			go workerSingleFlyght(ch, i, cli)
+		}
 	}
 
 	for _, url := range urls.Url {
@@ -115,7 +147,10 @@ func main() {
 		case ch <- url:
 		case <-time.After(3 * time.Second):
 			// チャネルの受信側が存在しなくなる or サーバ側が高負荷なので終了させる
-			log.Fatal("Timeout")
+			log.Fatal(Bold(Red("[ERROR] channel sending Timeout")))
+		case <-sig:
+			// キーボード割り込みとその他ネットワーク関連のシグナルは全てエラーとする
+			log.Fatal(Bold(Red("[ERROR] signal recved")))
 		}
 	}
 	close(ch)
